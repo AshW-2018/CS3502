c6c803fdb12b64c402d36ded10fe45179b36ff31
tonys24 <suokunstar@gmail.com>
2019-06-22 19:03:34 -0500

    Create mutexlock.c

---
+++ mutexlock.c
@@ -0,0 +1,36 @@
+/* compile: gcc xxx.c -o xxx.o -pthread  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int counter = 0;
+static pthread_mutex_t mlock;
+
+void *compute()
+{
+    int i = 0;
+    pthread_mutex_lock(&mlock);
+    while (i < 10000) {
+        counter = counter + 1;
+        i++;
+    }
+    pthread_mutex_unlock(&mlock);
+    printf("Counter value: %d\n", counter);
+}
+
+int main()
+{
+    pthread_t thread1, thread2, thread3, thread4, thread5;
+
+    pthread_create(&thread1, NULL, compute, (void *)&thread1);
+    pthread_create(&thread2, NULL, compute, (void *)&thread2);
+    pthread_create(&thread3, NULL, compute, (void *)&thread3);
+    pthread_create(&thread4, NULL, compute, (void *)&thread4);
+    pthread_create(&thread5, NULL, compute, (void *)&thread5);
+
+    pthread_exit(NULL);
+    exit(0);
+}
+
+
