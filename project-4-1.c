eb62e6ea246f4b6dfa7860994e525c05ed168a74
kevin <suokunstar@gmail.com>
2019-07-13 14:16:42 -0500

    Update project-4-1.c

--- project-4-1.c
+++ project-4-1.c
@@ -1,16 +1,17 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int init_module(void)
+
+int init_new_module(void)
 {
-    printk(KERN_INFO "Hello, world!\n");
-    return 0;
+	printk(KERN_INFO "Hello, world!\n");
+	return 0;
+
 }
 
-void cleanup_module(void)
-{
-    printk(KERN_INFO "Goodbye, world!\n");
+void exit_new_module(void) {
+	printk(KERN_INFO "Goodbye, world!\n");
 }
 
-module_init(init_module);
-module_exit(cleanup_module);
+module_init(init_new_module);
+module_exit(exit_new_module);
