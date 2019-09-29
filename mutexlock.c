2dd7ed7b16fa235fe2c49d6af3a082c2e082ec83
kevin <suokunstar@gmail.com>
2019-09-29 11:46:34 -0400

    .

--- mutexlock.c
+++ mutexlock.c
@@ -22,6 +22,11 @@ void *compute()
 int main()
 {
     pthread_t thread1, thread2, thread3, thread4, thread5;
+    if (pthread_mutex_init(&mlock, NULL) != 0)
+    {
+        printf("mutex init failed\n");
+        return 1;
+    }
 
     pthread_create(&thread1, NULL, compute, (void *)&thread1);
     pthread_create(&thread2, NULL, compute, (void *)&thread2);
@@ -30,7 +35,6 @@ int main()
     pthread_create(&thread5, NULL, compute, (void *)&thread5);
 
     pthread_exit(NULL);
+    pthread_mutex_destroy(&mlock);
     exit(0);
 }
-
-
