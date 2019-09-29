b93c76f4d8568d60ceecc70ff468e721c01f01d7
kevin <suokunstar@gmail.com>
2019-09-29 13:14:35 -0400

    .

---
+++ trylock.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main(void)
+{
+	int i = 0;
+	int res;
+
+	pthread_mutex_t lock;
+	pthread_mutex_init(&lock, NULL);
+
+	pthread_mutex_trylock(&lock);
+	if((res = pthread_mutex_trylock(&lock) != 0)) {
+		printf("don't lock\n");
+	} else {
+		printf("locked\n");
+	}
+
+	return 0;
+}
+
+
+
